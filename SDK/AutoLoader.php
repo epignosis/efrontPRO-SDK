e16c97faf539823bfd4c53f62ea5a0b28ec25667
xarhsdev <xarhs.dev@hotmail.com>
2015-02-19 10:58:17 +0200

    Convert line separators to UNIX style.

--- SDK/AutoLoader.php
+++ SDK/AutoLoader.php
@@ -2,7 +2,6 @@
 
 define('DOCUMENT_ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
-
 spl_autoload_register (function ($class) {
   $file = str_replace (
     '\\', DIRECTORY_SEPARATOR, DOCUMENT_ROOT . $class . '.php'
