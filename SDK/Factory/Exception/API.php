aac2c4f1b2a4263f06aba0f3c0cf13c94763f940
xarhsdev <xarhs.dev@hotmail.com>
2015-02-19 11:03:50 +0200

    QA Improvement.

--- SDK/Factory/Exception/API.php
+++ SDK/Factory/Exception/API.php
@@ -3,6 +3,13 @@
 namespace Factory\Exception;
 
 
+/**
+ * Class API
+ *
+ * @package   Factory\Exception
+ * @author    EPIGNOSIS
+ *
+ */
 final class API extends \Exception
 {
 
--- SDK/Request/Exception/cURL.php
+++ SDK/Request/Exception/cURL.php
@@ -3,6 +3,13 @@
 namespace Request\Exception;
 
 
+/**
+ * Class cURL
+ *
+ * @package   Request\Exception
+ * @author    EPIGNOSIS
+ *
+ */
 final class cURL extends \Exception
 {
 
