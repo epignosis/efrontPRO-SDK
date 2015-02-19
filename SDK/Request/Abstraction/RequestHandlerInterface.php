5d619852a0003eb3e103b66fc5769fb9119b7b81
xarhsdev <xarhs.dev@hotmail.com>
2015-02-19 11:15:01 +0200

    QA Improvement.

--- SDK/Request/Abstraction/RequestHandlerInterface.php
+++ SDK/Request/Abstraction/RequestHandlerInterface.php
@@ -2,14 +2,91 @@
 
 namespace Request\Abstraction;
 
-
+/**
+ * Interface RequestHandlerInterface
+ *
+ * @package   Request\Abstraction
+ * @author    EPIGNOSIS
+ *
+ */
 interface RequestHandlerInterface
 {
+  /**
+   * Closes the request handler.
+   *
+   * @return   $this
+   *
+   */
   public function Close();
+
+  /**
+   * Executes an HTTP/GET request and returns its response.
+   *
+   * @param    string $url    (Required) | The HTTP/GET URL.
+   * @param    string $apiKey (Required) | The API key.
+   *
+   * @return   string
+   *
+   */
   public function Get($url, $apiKey);
+
+  /**
+   * Initializes the request handler.
+   *
+   * @param   string $sdkVersion (Required) | The SDK version to be
+   *                                          used.
+   *
+   * @return  $this
+   *
+   */
   public function Init($sdkVersion);
+
+  /**
+   * Executes an HTTP/POST request and returns its response.
+   *
+   * @param    string $url               (Required)     | The
+   *                                                      HTTP/POST
+   *                                                      URL.
+   * @param    string $apiKey            (Required)     | The API key.
+   * @param    array  $postParameterList (Optional, []) | The POST
+   *                                                      parameter
+   *                                                      list.
+   *
+   * @return   string
+   *
+   */
   public function Post($url, $apiKey, array $postParameterList = []);
+
+  /**
+   * Executes an HTTP/POST request and returns its response.
+   *
+   * @param    string $url              (Required)     | The HTTP/PUT
+   *                                                     URL.
+   * @param    string $apiKey           (Required)     | The API key.
+   * @param    array  $putParameterList (Optional, []) | The PUT
+   *                                                     parameter
+   *                                                     list.
+   *
+   * @return   string
+   *
+   */
   public function Put($url, $apiKey, array $putParameterList = []);
+
+  /**
+   * Resets the request handler.
+   *
+   * @return   $this
+   *
+   */
   public function Reset();
-  public function SetOptionList(array $optionList = []);
+
+  /**
+   * Set the options for the referenced request handler.
+   *
+   * @param   array $optionList (Required) | The option list.
+   *
+   * @return  $this
+   *
+   */
+  public function SetOptionList(array $optionList);
 }
