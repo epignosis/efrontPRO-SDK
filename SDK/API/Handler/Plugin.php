67900b7c07cccf1d6fac64480a5b56b33bd9150e
xarhsdev <xarhs.dev@hotmail.com>
2015-02-20 11:11:17 +0200

    Update Plugin Handler. Add [POST] /Plugin/:pluginName.

--- SDK/API/Handler/Plugin.php
+++ SDK/API/Handler/Plugin.php
@@ -13,10 +13,31 @@ use API\Abstraction\AbstractAPI;
  */
 class Plugin extends AbstractAPI
 {
+
+  /**
+   * Validates the plugin name.
+   *
+   * @param   string $pluginName (Required) | The plugin name to be
+   *                                          checked.
+   *
+   * @throws  \Exception
+   *
+   * @return  $this
+   *
+   */
+  private function _CheckPluginName($pluginName)
+  {
+    if (empty($pluginName)) {
+      throw new \Exception('Plugin.InvalidName');
+    }
+
+    return $this;
+  }
+
   /**
    * Returns a list of the available plugins and their information.
    *
-   * @throws  \Exception (On network failure)
+   * @throws  \Exception
    *
    * @return  array (Associative)
    *
@@ -34,19 +55,42 @@ class Plugin extends AbstractAPI
    * @param   string $pluginName (Required) | The plugin name to
    *                                          retrieve its info.
    *
-   * @throws  \Exception (On network failure)
+   * @throws  \Exception
    *
    * @return  array (Associative)
    *
    */
   public function GetInfo($pluginName)
   {
-    if (empty($pluginName)) {
-      throw new \Exception('Plugin.InvalidName');
-    }
+    $this->_CheckPluginName($pluginName);
 
     return $this->_requestHandler->Get (
       $this->_GetAPICallURL('/Plugin/' . $pluginName), $this->_apiKey
+    );
+  }
+
+  /**
+   * Posts the requested data to be used by the requested plugin.
+   *
+   * @param   string $pluginName (Required) | The plugin name to
+   *                                          retrieve its info.
+   *
+   * @param   array  $data       (Required) | The data to be send to
+   *                                          the requested plugin.
+   *
+   * @throws  \Exception
+   *
+   * @return  array (Associative)
+   *
+   */
+  public function Notify($pluginName, array $data)
+  {
+    $this->_CheckPluginName($pluginName);
+
+    return $this->_requestHandler->Post (
+      $this->_GetAPICallURL('/Plugin/' . $pluginName),
+      $this->_apiKey,
+      $data
     );
   }
 }
