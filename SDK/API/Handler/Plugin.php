c0760d04b54f86e1a1f2decc25dc223645c5512b
xarhsdev <xarhs.dev@hotmail.com>
2015-02-20 10:48:05 +0200

    Update Plugin Handler. Add /Plugin/:pluginName.

--- SDK/API/Handler/Plugin.php
+++ SDK/API/Handler/Plugin.php
@@ -27,4 +27,26 @@ class Plugin extends AbstractAPI
       $this->_GetAPICallURL('/Plugins'), $this->_apiKey
     );
   }
+
+  /**
+   * Returns information about the requested plugin.
+   *
+   * @param   string $pluginName (Required) | The plugin name to
+   *                                          retrieve its info.
+   *
+   * @throws  \Exception (On network failure)
+   *
+   * @return  array (Associative)
+   *
+   */
+  public function GetInfo($pluginName)
+  {
+    if (empty($pluginName)) {
+      throw new \Exception('Plugin.InvalidName');
+    }
+
+    return $this->_requestHandler->Get (
+      $this->_GetAPICallURL('/Plugin/' . $pluginName), $this->_apiKey
+    );
+  }
 }
