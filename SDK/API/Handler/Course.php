f26048ac571df4f3bb6e19908d9f7ab67663f2ff
xarhsdev <xarhsdev@outlook.com>
2015-09-29 16:52:33 +0300

    [GET] /course/:id/training-sessions

--- SDK/API/Handler/Course.php
+++ SDK/API/Handler/Course.php
@@ -31,4 +31,23 @@ class Course extends AbstractAPI
       $this->_GetAPICallURL('/Course/' . $id), $this->_apiKey
     );
   }
+
+  /**
+   * Returns a list of the training sessions that belongs to the requested course.
+   *
+   * @param   mixed $id (Required) | The course identifier.
+   *
+   * @throws  \Exception
+   *
+   * @return  array (Associative)
+   *
+   */
+  public function GetTrainingSessions($id)
+  {
+    $this->_CheckId($id);
+
+    return $this->_requestHandler->Get (
+      $this->_GetAPICallURL('/course/' . $id . '/training-sessions'), $this->_apiKey
+    );
+  }
 }
