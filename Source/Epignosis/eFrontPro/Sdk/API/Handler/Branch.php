fcdf9c98be7db15cf0a59ad9ee24a76ea71447a6
xarhsdev <xarhsdev@outlook.com>
2016-01-08 15:28:36 +0200

    2.0.0

--- Source/Epignosis/eFrontPro/Sdk/API/Handler/Branch.php
+++ Source/Epignosis/eFrontPro/Sdk/API/Handler/Branch.php
@@ -15,6 +15,25 @@ use Epignosis\eFrontPro\Sdk\API\Abstraction\AbstractAPI;
 class Branch extends AbstractAPI
 {
   /**
+   * Creates a branch.
+   *
+   * @since 1.0.0
+   *
+   * @param array $branchInfo (Required) | The branch information.
+   *
+   * @throws \Exception
+   *
+   * @return array (Associative)
+   *
+   */
+  public function Create($branchInfo)
+  {
+    return $this->_requestHandler->Post (
+        $this->_GetAPICallURL('/Branch'), $this->_apiKey, $branchInfo
+    );
+  }
+
+  /**
    * Returns information about the requested branch.
    *
    * @since 1.0.0
