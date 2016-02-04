128d5ceb34ce67cbad017191a74824da0ff0b14e
xarhsdev <xarhsdev@users.noreply.github.com>
2016-02-04 17:16:25 +0200

    Remove autoloader exception #12

--- Source/AutoLoader.php
+++ Source/AutoLoader.php
@@ -3,18 +3,11 @@
 define('DOCUMENT_ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
 spl_autoload_register (function ($class) {
-  //I will only try to load my classes
-  if ( strpos($class, "Epignosis\\eFrontPro\\Sdk\\")!==0 ) {
-    return;
-  }
-
   $file = str_replace (
     '\\', DIRECTORY_SEPARATOR, DOCUMENT_ROOT . $class . '.php'
   );
 
-  if (!file_exists($file)) {
-    throw new \RuntimeException($file);
+  if (file_exists($file)) {
+    include $file;
   }
-
-  include $file;
 });
