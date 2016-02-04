abc265b0b964eead59ac5b7241febbf721acff20
Vassilis <poursal@gmail.com>
2016-02-04 10:44:47 +0200

    Autoloader will only load SDK specific classes.

--- Source/AutoLoader.php
+++ Source/AutoLoader.php
@@ -3,6 +3,11 @@
 define('DOCUMENT_ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
 spl_autoload_register (function ($class) {
+  //I will only try to load my classes
+  if ( strpos($class, "Epignosis\\eFrontPro\\Sdk\\")!==0 ) {
+    return;
+  }
+
   $file = str_replace (
     '\\', DIRECTORY_SEPARATOR, DOCUMENT_ROOT . $class . '.php'
   );
