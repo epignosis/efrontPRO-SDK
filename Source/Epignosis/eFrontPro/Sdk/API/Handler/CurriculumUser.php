645a5be8c807698e0f24932887e42ee43fe3db4b
xarhsdev <xarhsdev@outlook.com>
2016-04-19 11:45:07 +0300

    [Add] Curriculum/:Id/AddUser [Add] Curriculum/:Id/RemoveUser

---
+++ Source/Epignosis/eFrontPro/Sdk/API/Handler/CurriculumUser.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Epignosis\eFrontPro\Sdk\API\Handler;
+
+use Epignosis\eFrontPro\Sdk\API\Abstraction\AbstractAPI;
+
+/**
+ * Class CurriculumUser
+ *
+ * @author  EPIGNOSIS
+ * @package Epignosis\eFrontPro\Sdk
+ * @since   1.0.0
+ * @version 2.0.0
+ */
+class CurriculumUser extends AbstractAPI
+{
+  /**
+   * Creates a relation between the requested user and curriculum.
+   *
+   * @since 1.0.0
+   *
+   * @param mixed $userId       (Required) | The user identifier.
+   * @param mixed $curriculumId (Required) | The curriculum identifier.
+   * @param bool  $force        (Required) | Whether to force the operation.
+   *
+   * @throws \Exception
+   *
+   * @return array (Associative)
+   */
+  public function AddRelation($userId, $curriculumId, $force = false)
+  {
+    $this->_CheckId($userId)->_CheckId($curriculumId);
+
+    return $this->_requestHandler->Put (
+      $this->_GetAPICallURL('/Curriculum/' . $curriculumId . '/AddUser'),
+      $this->_apiKey,
+      ['UserId' => $userId, 'Force' => $force]
+    );
+  }
+
+  /**
+   * Removes the relation between the requested user and the curriculum.
+   *
+   * @since 1.0.0
+   *
+   * @param mixed $userId       (Required) | The user identifier.
+   * @param mixed $curriculumId (Required) | The curriculum identifier.
+   *
+   * @throws \Exception
+   *
+   * @return array (Associative)
+   */
+  public function RemoveRelation($userId, $curriculumId)
+  {
+    $this->_CheckId($userId)->_CheckId($curriculumId);
+
+    return $this->_requestHandler->Put (
+      $this->_GetAPICallURL('/Curriculum/' . $curriculumId . '/RemoveUser'),
+      $this->_apiKey,
+      ['UserId' => $userId]
+    );
+  }
+}
