047fb53b44e52a0ddc9776c6e06b3761bc74f146
xarhsdev <xarhsdev@outlook.com>
2017-03-14 11:52:21 +0200

    Add Job and JobList

---
+++ Source/Epignosis/eFrontPro/Sdk/API/Handler/Job.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Epignosis\eFrontPro\Sdk\API\Handler;
+
+use Epignosis\eFrontPro\Sdk\API\Abstraction\AbstractAPI;
+
+/**
+ * Class Job
+ *
+ * @author  EPIGNOSIS
+ * @package Epignosis\eFrontPro\Sdk
+ * @since   1.0.0
+ * @version 2.1.0
+ */
+class Job extends AbstractAPI
+{
+  /**
+   * Creates a job.
+   *
+   * @param   array $jobInfo (Required) | The account information.
+   *
+   * @throws  \Exception
+   *
+   * @return  array (Associative)
+   *
+   */
+  public function Create(array $jobInfo)
+  {
+    return $this->_requestHandler->Post (
+      $this->_GetAPICallURL('/Job'), $this->_apiKey, $jobInfo
+    );
+  }
+
+  /**
+   * Returns information about the requested job.
+   *
+   * @since 1.0.0
+   *
+   * @param mixed $id (Required) | The job identifier.
+   *
+   * @throws \Exception
+   *
+   * @return array (Associative)
+   */
+  public function GetInfo($id)
+  {
+    $this->_CheckId($id);
+
+    return $this->_requestHandler->Get (
+      $this->_GetAPICallURL('/Job/' . $id), $this->_apiKey
+    );
+  }
+}
---
+++ Source/Epignosis/eFrontPro/Sdk/API/Handler/JobList.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Epignosis\eFrontPro\Sdk\API\Handler;
+
+use Epignosis\eFrontPro\Sdk\API\Abstraction\AbstractAPI;
+
+/**
+ * Class JobList
+ *
+ * @author  EPIGNOSIS
+ * @package Epignosis\eFrontPro\Sdk
+ * @since   1.0.0
+ * @version 2.1.0
+ */
+class JobList extends AbstractAPI
+{
+  /**
+   * Returns the complete list of job.
+   *
+   * @since 1.0.0
+   *
+   * @throws \Exception
+   *
+   * @return array (Associative)
+   */
+  public function GetAll()
+  {
+    return $this->_requestHandler->Get (
+      $this->_GetAPICallURL('/Jobs'), $this->_apiKey
+    );
+  }
+}
