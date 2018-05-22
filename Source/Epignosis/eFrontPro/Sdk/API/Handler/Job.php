2bed2371f45df45a2bdcaddcffa2ad43d1edac4f
ryan-dervakos <ryan@efrontlearning.com>
2018-05-22 13:19:12 +0300

    Added Update method for Jobs

--- Source/Epignosis/eFrontPro/Sdk/API/Handler/Job.php
+++ Source/Epignosis/eFrontPro/Sdk/API/Handler/Job.php
@@ -39,7 +39,7 @@ class Job extends AbstractAPI
      * @return mixed
      */
     public function Update($id, array $jobInfo) {
-        return $this->_requestHandler->Put(
+        return $this->_requestHandler->Post(
             $this->_GetAPICallURL('/Job/'.$id),
             $this->_apiKey,
             $jobInfo);
