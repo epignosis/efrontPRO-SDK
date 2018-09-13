e9d7b7efe1ee8d758a2386c9310b835c2dd05a1c
ryan-dervakos <ryan@efrontlearning.com>
2018-09-13 14:09:29 +0300

    Fixes #26

---
+++ Source/Epignosis/eFrontPro/Sdk/API/Handler/Content.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Epignosis\eFrontPro\Sdk\API\Handler;
+
+use Epignosis\eFrontPro\Sdk\API\Abstraction\AbstractAPI;
+
+/**
+ * Class Content
+ *
+ * @author  EPIGNOSIS
+ * @package Epignosis\eFrontPro\Sdk
+ * @since   1.0.0
+ */
+class Content extends AbstractAPI
+{
+    /**
+     * Returns information about the requested content.
+     *
+     * @since 3.2.0
+     *
+     * @param mixed $id (Required) | The content identifier.
+     *
+     * @throws \Exception
+     *
+     * @return array (Associative)
+     */
+    public function GetInfo($id) {
+        $this->_CheckId($id);
+
+        return $this->_requestHandler->Get(
+            $this->_GetAPICallURL('/Content/'.$id),
+            $this->_apiKey
+        );
+    }
+
+}
